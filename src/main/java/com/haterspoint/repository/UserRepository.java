a7b80154fcda29cb21ac589285f951fa59f806e6
Sravanthi <shravan.527@gmail.com>
2020-04-07 15:50:54 -0400

    added code

--- src/main/java/com/haterspoint/repository/UserRepository.java
+++ src/main/java/com/haterspoint/repository/UserRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Serializable> {
     List<UserEntity> findByEmailId(String emailId);
-    List<UserEntity> findByPassword(String password);
+
+
 }
--- src/main/java/com/haterspoint/service/impl/BrandServiceImpl.java
+++ src/main/java/com/haterspoint/service/impl/BrandServiceImpl.java
@@ -1,4 +1,54 @@
 package com.haterspoint.service.impl;
 
-public class BrandServiceImpl {
+
+
+import com.haterspoint.dto.Brand;
+import com.haterspoint.service.BrandService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.ArrayList;
+@Service
+
+public class BrandServiceImpl implements BrandService{
+   @Override
+
+
+   public List<Brand> getBrands() {
+
+                   List<Brand> brands = new ArrayList<>();
+
+                for (int i = 0; i < 40; i++) {
+                     Brand brand = new Brand();
+
+                       brand.setId(i);
+                     brand.setName("brand" + i);
+                    brand.setNoOfAngry(i + 1);
+                  brand.setNoOfFrustrations(i + 5);
+
+                            brands.add(brand);
+
+                       }
+        return brands;
+          }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
